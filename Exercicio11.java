b2ebd53eeaae571a9e56a6e6f952086131f057c6
vitor <almeida.vitor20000@gmail.com>
2024-03-24 17:25:49 -0300

    11 Resolvido

---
+++ Exercicio11.java
@@ -0,0 +1,13 @@
+/*Escreva um algoritmo que leia três valores inteiros distintos e escreva-os em
+ordem crescente.  */
+
+public class Exercicio11 {
+
+    public static void executar() {
+
+        for (int i = 10; i < 13; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
