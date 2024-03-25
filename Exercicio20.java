81c330c9ae96a8da4813d2102acdc492bd50a77f
vitor <almeida.vitor20000@gmail.com>
2024-03-25 01:53:44 -0300

    20 resolvido, exercicios01 finalizados

---
+++ Exercicio20.java
@@ -0,0 +1,22 @@
+/*Elabore um algoritmo que calcule a quantidade de litros de combustível gasta em
+uma viagem, utilizando um automóvel que faz 12km por litro e considerando que são
+fornecidos o tempo em hora e a velocidade média da viagem. */
+
+import java.util.Scanner;
+
+public class Exercicio20 {
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual é o tempo da viagem: ");
+        double tempo = scanner.nextDouble();
+
+        System.out.println("Qual é a velocidade média da viagem: ");
+        double vm = scanner.nextDouble();
+
+        double litros = tempo * vm;
+
+        System.out.println("A quantidade de combustível gasta na viagem foi " + litros / 12);
+
+    }
+
+}
