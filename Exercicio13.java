77feb04743f0ccee2c97014f8109519fd354a4bb
vitor <almeida.vitor20000@gmail.com>
2024-03-24 22:46:46 -0300

    13 Resolvido

--- Exercicio13.java
+++ Exercicio13.java
@@ -10,26 +10,39 @@ public class Exercicio13 {
     public static void executar() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Qual o operador utilizado: ");
-        String oper = scanner.nextLine();
-
+        System.out.println(
+                "Qual o operador utilizado ('+' para soma, '-' para subtracao, '/' para divisao, '*' multiplicacao): ");
+        char oper = scanner.next().charAt(0);
+        if ("+-/*".indexOf(oper) == -1) {
+            System.out.println("Operacao [" + oper + "] nao suportada");
+            scanner.close();
+            return;
+        }
         System.out.println("Digite um número");
         double A = scanner.nextDouble();
 
         System.out.println("Digite outro número");
         double B = scanner.nextDouble();
 
+        scanner.close();
+
         switch (oper) {
-            case "adição":
-                oper = "Adição";
-                System.out.println("Adição");
-
+            case '+':
+                System.out.println("O resultado da soma de " + A + "+" + "B eh" + A + B);
                 break;
-
+            case '-':
+                System.out.println("O resultado da subtracao de " + A + "-" + "B eh" + (A - B));
+                break;
+            case '/':
+                System.out.println("O resultado da divisao de " + A + "/" + " B é " + A / B);
+                break;
+            case '*':
+                System.out.println("O resultado da multiplicacao de " + A + "*" + "B eh" + A * B);
+                break;
             default:
+                System.out.println("Se você chegou aqui, parabéns!");
                 break;
         }
 
     }
-
 }
